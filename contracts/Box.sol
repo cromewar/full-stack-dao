513b4e6e6c39e5ab93443232b6810b652ef03a7f
cromewar <vasiliy@cromewar.com>
2022-03-04 14:47:51 -0500

    box contract and governance token contract added

---
+++ contracts/Box.sol
@@ -0,0 +1,20 @@
+//SDPX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Box is Ownable {
+    uint256 private number;
+
+    event ValueChanged(uint256 newNumber);
+
+    function store(uint256 newNumber) public onlyOwner {
+        number = newNumber;
+        emit ValueChanged(newNumber);
+    }
+
+    function retreive() public view returns (uint256) {
+        return number;
+    }
+}
